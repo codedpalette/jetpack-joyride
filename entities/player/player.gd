c83f3b460899adf8f60bbbaaa15a90df8bc88f68
daniel <daniel.blindr@gmail.com>
2026-03-06 11:27:22 +0000

    Player sprite

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,12 +2,12 @@ extends CharacterBody2D
 
 @export_range(500, 2000, 1) var gravity := 1000.0
 @export_range(500, 2000, 1) var jump := 1200.0
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+#@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
-func _draw() -> void:
-    var rect := collision_shape.shape.get_rect()
-    draw_rect(rect, Color.GREEN)
-    draw_rect(rect, Color.BLACK, false, 2.0)
+# func _draw() -> void:
+#     var rect := collision_shape.shape.get_rect()
+#     draw_rect(rect, Color.GREEN)
+#     draw_rect(rect, Color.BLACK, false, 2.0)
 
 func _physics_process(delta: float) -> void:
     var acceleration := Vector2.DOWN * gravity
