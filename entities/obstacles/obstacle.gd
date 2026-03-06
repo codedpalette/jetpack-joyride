779559f353dd06a2d576f5bca0c47b30fcef8222
daniel <daniel.blindr@gmail.com>
2026-03-06 11:58:04 +0000

    Obstacles and player death

---
+++ entities/obstacles/obstacle.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+signal exited_screen
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+const speed := 400.0
+
+func _draw() -> void:
+    var rect := collision_shape.shape.get_rect()
+    draw_rect(rect, Color.RED)
+    draw_rect(rect, Color.BLACK, false, 2.0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    position.x -= speed * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+    if body is Player:
+        (body as Player).die()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+    exited_screen.emit()
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,17 +1,15 @@
+class_name Player
 extends CharacterBody2D
 
 @export_range(500, 2000, 1) var gravity := 1000.0
 @export_range(500, 2000, 1) var jump := 1200.0
-#@onready var collision_shape: CollisionShape2D = $CollisionShape2D
-
-# func _draw() -> void:
-#     var rect := collision_shape.shape.get_rect()
-#     draw_rect(rect, Color.GREEN)
-#     draw_rect(rect, Color.BLACK, false, 2.0)
 
 func _physics_process(delta: float) -> void:
     var acceleration := Vector2.DOWN * gravity
-    if Input.is_action_pressed("ui_accept"):
+    if Input.is_action_pressed("shoot"):
         acceleration = Vector2.UP * jump
     velocity += acceleration * delta
     move_and_slide()
+
+func die() -> void:
+    print("Player died")
