570c12415c4b4fe7af0d58671c3e678811a23f96
daniel <daniel.blindr@gmail.com>
2026-03-18 13:43:58 +0000

    Automatically draw obstacle spawner between world bounds

--- world/world.gd
+++ world/world.gd
@@ -1,15 +1,18 @@
+@tool
 class_name World
 extends Node2D
 
 @onready var player_track: Node2D = $PlayerTrack
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-    pass # Replace with function body.
+@onready var obstacle_spawner: Path2D = $ObstacleSpawner
+@onready var ceiling_body: StaticBody2D = $Ceiling
+@onready var floor_body: StaticBody2D = $Floor
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-    pass
+    if Engine.is_editor_hint():
+        obstacle_spawner.curve.set_point_position(0, ceiling_body.position + Vector2.DOWN * 20)
+        obstacle_spawner.curve.set_point_position(1, floor_body.position + Vector2.UP * 20)
+
 
 func add_player(player: Player) -> void:
     player_track.add_child(player)
