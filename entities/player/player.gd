766396d8b27e3d01446817c4d5da8fdb9297acd8
daniel <daniel.blindr@gmail.com>
2026-03-18 12:48:58 +0000

    Improve player physics

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,14 +1,23 @@
 class_name Player
 extends CharacterBody2D
 
-@export_range(500, 2000, 1) var gravity := 1000.0
-@export_range(500, 2000, 1) var jump := 1200.0
+const JUMP_FORCE := 2000.0
+const GRAVITY := 2000.0
+const MAX_VELOCITY := 1000.0
+
+func _input(event: InputEvent) -> void:
+    if event.is_action_pressed("shoot"):
+        velocity.y = 0
+
 
 func _physics_process(delta: float) -> void:
-    var acceleration := Vector2.DOWN * gravity
+    var acceleration := Vector2.ZERO
     if Input.is_action_pressed("shoot"):
-        acceleration = Vector2.UP * jump
+        acceleration = Vector2.UP * JUMP_FORCE
+    elif not is_on_floor():
+        acceleration = Vector2.DOWN * GRAVITY
     velocity += acceleration * delta
+    velocity.y = clamp(velocity.y, -MAX_VELOCITY, MAX_VELOCITY)
     move_and_slide()
 
 func die() -> void:
