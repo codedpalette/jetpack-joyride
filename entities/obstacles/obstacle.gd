5c02bfb0df36fc6041c6a79b2f042e587e8e1904
daniel <daniel.blindr@gmail.com>
2026-03-29 19:11:15 +0100

    obstacle sprite

--- entities/obstacles/obstacle.gd
+++ entities/obstacles/obstacle.gd
@@ -3,17 +3,15 @@ extends Area2D
 
 signal exited_screen
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite2D
 const speed := 400.0
-
-func _draw() -> void:
-    var rect := collision_shape.shape.get_rect()
-    draw_rect(rect, Color.RED)
-    draw_rect(rect, Color.BLACK, false, 2.0)
+const rotation_speed := 360.0 # degrees per second
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     position.x -= speed * delta
-
+    sprite.rotation_degrees -= rotation_speed * delta
+    
 
 func _on_body_entered(body: Node2D) -> void:
     if body is Player:
